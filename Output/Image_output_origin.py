e88433444134a6af77417976ffc54f51e84aabf3
sunmi choi <keunangyung01@naver.com>
2021-12-14 16:33:46 +0900

    Add Image_output_origin_py

---
+++ Output/Image_output_origin.py
@@ -0,0 +1,34 @@
+import time
+
+def Chorus_origin():
+    for i in range(2):
+        img1()
+        time.sleep(0.5)
+        img2()
+        time.sleep(0.5) 
+        img3()
+        time.sleep(1)
+        img3()
+        time.sleep(1)
+
+Chorus_origin
+img1()
+time.sleep(4)
+img2()
+time.sleep(4)
+img3()
+time.sleep(10)
+img4()
+time.sleep(4)
+img5()
+time.sleep(4)
+img6()
+time.sleep(5)
+img7()
+time.sleep(15)
+img8()
+time.sleep(4)
+img9()
+time.sleep(4)
+imge10()
+
