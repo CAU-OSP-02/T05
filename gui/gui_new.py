ec297389dfd895c7fadaebdee6eef426b2cb2b51
KIWOO2001 <yido2001@naver.com>
2021-12-19 22:37:53 +0900

    mos15

--- gui/gui_new.py
+++ gui/gui_new.py
@@ -70,21 +70,19 @@ class Cam(tk.Frame):
     
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.width, self.height = 300,200
         self.master = master
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
-        self.canvas = Canvas(master, width = self.width, height = self.height)
+        self.canvas = Canvas(master, width = 300, height = 200)
         self.canvas.place(x=520, y=210)
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
         self.update()
         self.detect()
         
     def update(self):
-        
-        hand = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)
-        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(hand))
-        self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
+        self.hand = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)
+        self.imgtk = PIL.ImageTk.PhotoImage(image = Image.fromarray(self.hand))
+        self.canvas.create_image(0, 0, image = self.imgtk, anchor = NW)
         self.master.after(20, self.update)
     
     def detect(self):
